f946e82d31f3dda5ff3a0875322d4e5af6681186
Alex Hand <alex.j.hand@gmail.com>
2018-10-16 12:10:20 -0500

    DisplayInformation css completed

--- client/src/components/ImageCarousel.js
+++ client/src/components/ImageCarousel.js
@@ -4,11 +4,22 @@ import SubImage from './SubImage';
 
 const ImageCarousel = (props) => 
     <div className="imageCarousel">
-        <div onClick={props.onClickLeft}>&#x3008;</div>
-        <SubImage image={props.subImage1} title={props.title} onClick={props.onClickLeft} />
-        <SubImage image={props.subImage2} title={props.title} />
-        <SubImage image={props.subImage3} title={props.title} onClick={props.onClickRight} />
-        <div onClick={props.onClickRight}>&#x3009;</div>
+        <div className="carouselItem">
+            <div className="carouselDirection" onClick={props.onClickLeft}>&#x3008;</div>
+        </div>
+        <div className="carouselItem">
+            <SubImage image={props.subImage1} title={props.title} onClick={props.onClickLeft} />
+        </div>
+        <div className="carouselItem">
+            <SubImage image={props.subImage2} title={props.title} />
+        </div>
+        <div className="carouselItem">
+            <SubImage image={props.subImage3} title={props.title} onClick={props.onClickRight} />
+        </div>
+        <div className="carouselItem">
+            <div className="carouselDirection" onClick={props.onClickRight}>&#x3009;</div>
+        </div>
+        
     </div>
 
 export default ImageCarousel;
